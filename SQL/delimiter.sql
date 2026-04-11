e496896a1c296a236a69127585bc761d3728e6c9
Sairaj-Serigara <sairajsherigar5@gmail.com>
2026-04-11 20:51:29 +0530

    added procedure with parameter

--- SQL/delimiter.sql
+++ SQL/delimiter.sql
@@ -14,3 +14,18 @@ END //
 delimiter ;
 
 call list_of_employees();
+
+-- Stored Procedure with Parameters
+
+delimiter //
+
+CREATE PROCEDURE get_employee_by_id(IN id INT)
+
+BEGIN
+SELECT * FROM employees WHERE employee_id=id;
+END //
+
+delimiter ;
+call get_employee_by_id(4);
+
+-- DROP PROCEDURE IF EXISTS get_employee_by_id
