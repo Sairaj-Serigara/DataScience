3d2dcd189f495d9094b40be7f45fa126754119b5
Sairaj-Serigara <sairajsherigar5@gmail.com>
2026-06-29 09:58:40 +0530

    Track Git and git_ingnore as normal folders

---
+++ Git/main.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.listdir())
+
+print(os.getcwd())
+
+#end of file.
+# this is simple simple branch 
---
+++ Git/readme.md
@@ -0,0 +1,4 @@
+#readme.md
+this is readme.md
+
+let me walk you through this
